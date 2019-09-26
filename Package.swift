9449b122eacc31bda2d35f4705012292b46feafa
Vitalii Obertynskyi <vober@icloud.com>
2019-09-26 14:31:44 +0300

    Added new parameters for packege

--- Package.swift
+++ Package.swift
@@ -1,11 +1,21 @@
-
-
+// swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(name: "VGSFramework",
-                      platforms: [.iOS(.v12)],
-                      products: [.library(name: "VGSFramework",
-                                          targets: ["VGSFramework"])],
-                      targets: [.target(name: "VGSFramework",
-                                        path: "framework")],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "VGSFramework",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(name: "VGSFramework", targets: ["VGSFramework"]),
+    ],
+    dependencies: [
+//        .package(url: "https://url/of/another/package/named/Utility", from: "1.0.0"),
+        .package(name: "SnapKit"),
+        .package(name: "Alamofire")
+    ],
+    targets: [
+        .target(name: "VGSFramework"),
+        .testTarget(name: "FrameworkTests", dependencies: ["VGSFramework"]),
+    ]
+)
