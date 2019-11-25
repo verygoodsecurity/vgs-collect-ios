94d6df4c4e4dc69db02718ca7c01ff32136e0671
dmytro.khl <dmytro.khludkov@vgs.io>
2019-11-25 18:30:05 +0200

    Clean project.

--- demoapp/demoapp/ViewController.swift
+++ demoapp/demoapp/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         }
     }
     // Collector vgs
-    var vgsForm = VGSCollect(id: "tntbgaa2ads", environment: .sandbox)
+    var vgsForm = VGSCollect(id: "<your-vault-id>", environment: .sandbox)
     
     // VGS UI Elements
     var cardNumber = VGSTextField()
