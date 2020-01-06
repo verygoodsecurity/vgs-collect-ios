e0f4556f7cdc9fd63dd7c9805df1e2f18ec02723
dmytro.khl <dmytro.khludkov@vgs.io>
2020-01-06 14:28:25 +0200

    Implement VGSScanController.

---
+++ framework/Sources/VGSFramework/UIElements/Scan/ScanProvider/VGSScanProvider.swift
@@ -0,0 +1,23 @@
+//
+//  VGSScanProvider.swift
+//  VGSFramework
+//
+//  Created by Dima on 06.01.2020.
+//  Copyright © 2020 VGS. All rights reserved.
+//
+
+import Foundation
+#if canImport(UIKit)
+import UIKit
+#endif
+
+public enum ScanProvider {
+    case cardIO
+    case cardScan
+}
+
+internal protocol VGSScanProviderProtocol {
+    var delegate: VGSScanControllerDelegate? { get set }
+    func presentScan(from viewController: UIViewController)
+    func dismissScan(animated: Bool, completion: (() -> Void)?)
+}
