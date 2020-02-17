6e65eb9e9f65d194fbe8a0264cdef120e46cb51c
dmytro.khl <dmytro.khludkov@vgs.io>
2020-02-17 20:15:56 +0200

    Update demo app with file picker.

--- demoapp/demoapp/FilePickerViewController.swift
+++ demoapp/demoapp/FilePickerViewController.swift
@@ -11,29 +11,81 @@ import VGSCollectSDK
 
 class FilePickerViewController: UIViewController {
 
-    var filePicker: VGSFilePickerController?
+    // Create strong referrence of VGSFilePickerController
+    @IBOutlet weak var stateLabel: UILabel!
+    var pickerController: VGSFilePickerController?
     // Collector vgs
     var vgsForm = VGSCollect(id: "tntdw5wjxet", environment: environment)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        stateLabel.text = "Select File to Upload!"
     }
     
     @IBAction func selectFileAction(_ sender: UIButton) {
-        
-        let filePickerConfig = VGSFilePickerConfiguration.init(collector: vgsForm, fieldName: "card_image", fileSource: .library)
-        filePicker = VGSFilePickerController.init(configuration: filePickerConfig)
-        filePicker?.present(on: self, animated: true, completion: nil)
+        selectFileFromSource()
     }
     
     @IBAction func submitAction(_ sender: Any) {
-        vgsForm.submitFile(path: "/post", method: .post) { (json, error) in
+        stateLabel.text = "Uploading file..."
+        vgsForm.submitFile(path: "/post", method: .post) { [weak self](json, error) in
             if error == nil, let json = json {
-
+                self?.stateLabel.text = "Success!!!\n" + (String(data: try! JSONSerialization.data(withJSONObject: json, options: .prettyPrinted), encoding: .utf8)!)
             } else {
                 print("Error: \(String(describing: error?.localizedDescription))")
+                self?.stateLabel.text = "Something when wrong!"
             }
         }
+    }
+    
+    private func selectFileFromSource() {
+        let alert = UIAlertController(title: "Source Type", message: "Where is file you need to upload?", preferredStyle: .actionSheet)
+
+        alert.addAction(UIAlertAction(title: "Photo Library", style: .default , handler:{ [weak self]  (UIAlertAction)in
+            self?.showPickerWithSource(.library)
+        }))
+
+        alert.addAction(UIAlertAction(title: "Camera", style: .default , handler:{ [weak self](UIAlertAction)in
+            self?.showPickerWithSource(.camera)
+        }))
+
+        alert.addAction(UIAlertAction(title: "Documents Directory", style: .default , handler:{ [weak self] (UIAlertAction)in
+            self?.showPickerWithSource(.documentsDirectory)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func showPickerWithSource(_ source: VGSFileSource) {
+        let fieldName = source == .documentsDirectory ? "secred_doc" : "card_image"
+        let filePickerConfig = VGSFilePickerConfiguration(collector: vgsForm, fieldName: fieldName, fileSource: source)
+        pickerController = VGSFilePickerController(configuration: filePickerConfig)
+        pickerController?.delegate = self
+        pickerController?.presentFilePicker(on: self, animated: true, completion: nil)
+    }
+    
+}
+
+extension FilePickerViewController: VGSFilePickerControllerDelegate {
+    func userDidPickFileWithInfo(_ info: VGSFileInfo) {
+        self.stateLabel.text = """
+                                File info:
+                                - fileExtension: \(info.fileExtension ?? "unknown")
+                                - size: \(info.size)
+                                - sizeUnit: \(info.sizeUnit ?? "unknown")
+                                """
+        pickerController?.dismissFilePicker(animated: true)
+    }
+    
+    func userDidSCancelFilePicking() {
+        pickerController?.dismissFilePicker(animated: true)
+    }
+    
+    func filePickingFailedWithError(_ error: String) {
+        self.stateLabel.text = "Select file error! Try again!"
+        pickerController?.dismissFilePicker(animated: true)
     }
 }
