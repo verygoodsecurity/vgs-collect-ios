ef2647e29c19265012e369c70bf169fae73b312d
dmytrokhl <57712402+dmytrokhl@users.noreply.github.com>
2020-02-27 16:32:53 +0200

    Feature/ch60834/validation requirements (#93)
    * Add isRequired fields to Configuration 
    
    * Update errors handling.

--- framework/Sources/VGSFramework/Core/VGSErrorInfoKey.swift
+++ framework/Sources/VGSFramework/Core/VGSErrorInfoKey.swift
@@ -19,4 +19,5 @@ public let VGSSDKErrorInputDataRequired: VGSErrorInfoKey = "VGSSDKErrorInputData
 public let VGSSDKErrorInputDataRequiredValid: VGSErrorInfoKey = "VGSSDKErrorInputDataRequiredValid"
 
 // Response errors
+
 public let VGSSDKErrorUnexpectedResponseDataFormat: VGSErrorInfoKey = "VGSSDKErrorUnexpectedResponseDataFormat"
