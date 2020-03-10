9f5c90ae60d47b2c64cfa651face5e318127dcf5
dmytro.khl <dmytro.khludkov@vgs.io>
2020-03-10 17:28:37 +0200

    Update merging textfield data with extradata.

--- framework/Sources/VGSFramework/Core/VGSCollect+internal.swift
+++ framework/Sources/VGSFramework/Core/VGSCollect+internal.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+internal extension VGSCollect {
+    
+    /// Validate tenant id
+    class func tenantIDValid(_ tenantId: String) -> Bool {
+        return tenantId.isAlphaNumeric
+    }
+    
+    /// Validate textfields input data
+    func validate(_ input: [VGSTextField]) -> Error? {
+        var isRequiredErrorFields = [String]()
+        var isRequiredValidOnlyErrorFields = [String]()
+        
+        for textField in input {
+            if textField.isRequired, textField.text.isNilOrEmpty {
+                isRequiredErrorFields.append(textField.fieldName)
+            }
+            if textField.isRequiredValidOnly && !textField.state.isValid {
+                isRequiredValidOnlyErrorFields.append(textField.fieldName)
+            }
+        }
+        
+        if isRequiredErrorFields.count > 0 {
+            return VGSError(type: .inputDataRequired, userInfo: VGSErrorInfo(key: VGSSDKErrorInputDataRequired, description: "Input data can't be nil or empty", extraInfo: ["fields": isRequiredErrorFields]))
+        } else if isRequiredValidOnlyErrorFields.count > 0 {
+            return VGSError(type: .inputDataRequiredValidOnly, userInfo: VGSErrorInfo(key: VGSSDKErrorInputDataRequiredValid, description: "Input data should be valid only", extraInfo: ["fields": isRequiredValidOnlyErrorFields]))
+        }
+        return nil
+    }
+    
+    func mapFieldsDataToDictionary(_ body: [String: Any]) -> [String: Any] {
+        var resultDict = [String: Any]()
+        for (key, value) in body {
+            let mappedDict = mapStringKVOToDictionary(key: key, value: value, separator: ".")
+            let newDict = deepMerge(resultDict, mappedDict)
+            resultDict = newDict
+        }
+        return resultDict
+    }
+}
+
--- framework/Sources/VGSFramework/Core/VGSCollect.swift
+++ framework/Sources/VGSFramework/Core/VGSCollect.swift
@@ -90,17 +90,17 @@ extension VGSCollect {
             return
         }
 
-        let body: BodyData = elements.reduce(into: BodyData()) { (dict, element) in
+        let textFieldsData: BodyData = elements.reduce(into: BodyData()) { (dict, element) in
             dict[element.fieldName] = element.rawText
         }
         
-        var mappedData = mapNestingObjects(body)
+        var body = mapFieldsDataToDictionary(textFieldsData)
         
-        if extraData?.count != 0 {
-            extraData?.forEach { (key, value) in mappedData[key] = value }
+        if let customData = extraData, customData.count != 0 {
+            body = deepMerge(customData, body)
         }
                 
-        apiClient.sendRequest(path: path, method: method, value: mappedData) { (json, error) in
+        apiClient.sendRequest(path: path, method: method, value: body) { (json, error) in
             
             if let error = error {
                 block(json, error)
@@ -117,71 +117,4 @@ extension VGSCollect {
             }
         }
     }
-    
-    private func mapNestingObjects(_ body: [String: Any]) -> [String: Any] {
-        
-        var resultDict = [String: Any]()
-        for (key, value) in body {
-            
-            let components = key.split(separator: ".").map{ String($0) }
-            var currentDict = [String: Any]()
-            
-            var i = components.count - 1
-            
-            while i >= 0 {
-                if i == components.count - 1 {
-                    currentDict[components[i]] = value
-                } else {
-                    let dict = [components[i] : currentDict]
-                    currentDict = dict
-                }
-                i -= 1
-            }
-            let newDict = deepMerge(resultDict, currentDict)
-            resultDict = newDict
-        }
-        return resultDict
-    }
 }
-
-// MARK: - Validation
-internal extension VGSCollect {
-    
-    func validate(_ input: [VGSTextField]) -> Error? {
-        var isRequiredErrorFields = [String]()
-        var isRequiredValidOnlyErrorFields = [String]()
-        
-        for textField in input {
-            if textField.isRequired, textField.text.isNilOrEmpty {
-                isRequiredErrorFields.append(textField.fieldName)
-            }
-            if textField.isRequiredValidOnly && !textField.state.isValid {
-                isRequiredValidOnlyErrorFields.append(textField.fieldName)
-            }
-        }
-        
-        if isRequiredErrorFields.count > 0 {
-            return VGSError(type: .inputDataRequired, userInfo: VGSErrorInfo(key: VGSSDKErrorInputDataRequired, description: "Input data can't be nil or empty", extraInfo: ["fields": isRequiredErrorFields]))
-        } else if isRequiredValidOnlyErrorFields.count > 0 {
-            return VGSError(type: .inputDataRequiredValidOnly, userInfo: VGSErrorInfo(key: VGSSDKErrorInputDataRequiredValid, description: "Input data should be valid only", extraInfo: ["fields": isRequiredValidOnlyErrorFields]))
-        }
-        return nil
-    }
-    
-    class func tenantIDValid(_ tenantId: String) -> Bool {
-        return tenantId.isAlphaNumeric
-    }
-    
-    func deepMerge(_ d1: [String: Any], _ d2: [String:Any]) -> [String:Any] {
-        var result = d1
-        for (k2, v2) in d2 {
-            if let v2 = v2 as? [String:Any], let v1 = result[k2] as? [String:Any] {
-                result[k2] = deepMerge(v1, v2)
-            } else {
-                result[k2] = v2
-            }
-        }
-        return result
-    }
-}
-
