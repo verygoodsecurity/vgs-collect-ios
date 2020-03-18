dd022de063bef723c5db8ae4030769f14bc6f959
Vitalii Obertynskyi <vober@icloud.com>
2020-03-18 12:10:02 +0200

    Update VGSTextField+patternFormat.swift

--- framework/Sources/VGSFramework/UIElements/Text Field/VGSTextField+patternFormat.swift
+++ framework/Sources/VGSFramework/UIElements/Text Field/VGSTextField+patternFormat.swift
@@ -12,35 +12,6 @@ extension VGSTextField {
     var formatPatternForCard: String {
         let defPattern = "#### #### #### ####"
         return defPattern
-// TBD: -
-//        guard let text = textField.getSecureRawText else {
-//            return defPattern
-//        }
-//
-//        let clearText = text.components(separatedBy: " ").joined(separator: "")
-//
-//        guard let state = state as? CardState else {
-//            return defPattern
-//        }
-//
-//        let cardBrand = state.cardBrand
-//
-//        switch clearText.count {
-//        case 4...14:
-//            return "#### ###### ####"
-//
-//        case 15:
-//            return "#### ###### #####"
-//
-//        case 16:
-//            return defPattern
-//
-//        case 17...19:
-//            return "#### #### #### #### ###"
-//
-//        default:
-//            return defPattern
-//        }
     }
     
     var formatPatternForCvc: String {
