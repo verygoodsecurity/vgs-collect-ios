d66a077ac50f1796b18f368584711bd4cbe198ad
dmytro.khl <dmytro.khludkov@vgs.io>
2020-04-09 16:13:04 +0300

    Add more validation cases.

--- framework/Tests/FrameworkTests/Card Brand Tests/CardBrandTest.swift
+++ framework/Tests/FrameworkTests/Card Brand Tests/CardBrandTest.swift
@@ -9,7 +9,7 @@
 import XCTest
 @testable import VGSFramework
 
-class MasterCardBrandTest: XCTestCase {
+class CardBrandTest: XCTestCase {
     var storage: VGSCollect!
     var cardTextField: VGSTextField!
     
@@ -19,7 +19,7 @@ class MasterCardBrandTest: XCTestCase {
         
         let config = VGSConfiguration(collector: storage, fieldName: "cardNum")
         config.type = .cardNumber
-        
+        config.formatPattern = nil
         cardTextField.configuration = config
     }
 
@@ -28,7 +28,7 @@ class MasterCardBrandTest: XCTestCase {
         cardTextField = nil
     }
     
-    func testAllBrand() {
+    func testCardBrandDetectionReturnsTrue() {
         let allBrands = SwiftLuhn.CardType.allCases
         allBrands.forEach { brand in
             let numbers = brand.cardNumbers
@@ -40,7 +40,100 @@ class MasterCardBrandTest: XCTestCase {
                 }
                 XCTAssert(state.cardBrand == brand, "Card number \(number) for brand \(brand) fail")
             }
-            
+        }
+    }
+    
+    func testCardBrandDetectionByFirstDigitsReturnsTrue() {
+        let allBrands = SwiftLuhn.CardType.allCases
+        allBrands.forEach { brand in
+            let numbers = brand.cardNumbers
+            numbers.forEach { number in
+                let startIndex = (brand == .mastercard || brand == .maestro) ? 7 : 4
+                let digitsCount = Int.random(in: startIndex...11)
+                let input = String(number.prefix(digitsCount))
+                cardTextField.textField.secureText = input
+                guard let state = cardTextField.state as? CardState else {
+                    XCTFail("Guard fail")
+                    return
+                }
+                XCTAssert(state.cardBrand == brand, "Card number \(number), first digits \(input), for brand \(brand) fail")
+            }
+        }
+    }
+    
+    func testValidCardsValidationReturnsTrue() {
+        let allBrands = SwiftLuhn.CardType.allCases
+        allBrands.forEach { brand in
+            let numbers = brand.cardNumbers
+            numbers.forEach { number in
+                cardTextField.textField.secureText = number
+                guard let state = cardTextField.state as? CardState else {
+                    XCTFail("Guard fail")
+                    return
+                }
+
+                XCTAssert(state.isValid == true, "Card number \(number) for brand \(brand) fail")
+            }
+        }
+    }
+    
+    func testNotFullCardsValidationReturnsFalse() {
+        let allBrands = SwiftLuhn.CardType.allCases
+        allBrands.forEach { brand in
+            let numbers = brand.cardNumbers
+            for number in numbers {
+                if number.count > 16 {
+                    continue
+                }
+                
+                let digitsCount = Int.random(in: 1...6)
+                
+                let input = String(number.prefix(number.count - digitsCount))
+                cardTextField.textField.secureText = input
+                guard let state = cardTextField.state as? CardState else {
+                    XCTFail("Guard fail")
+                    return
+                }
+                XCTAssert(state.isValid == false, "Card number \(number), first digits \(input), for brand \(brand) fail")
+            }
+        }
+    }
+    
+    func testNotValidCardsValidationReturnsFalse() {
+        let allBrands = SwiftLuhn.CardType.allCases
+        allBrands.forEach { brand in
+            let numbers = brand.cardNumbers
+            numbers.forEach { number in
+                /// replace rendom number in card. We skip first digits that detect brand since brand used for validation too.
+                let lastIndex = number.count > 16 ? 16 : number.count
+                let replaceCharIndex = Int.random(in: 7...lastIndex)
+                let prefix = number.prefix(replaceCharIndex)
+                let numToChange = Int(String(prefix.last!))!
+                var randomNums = Array(0...9)
+                randomNums.remove(at: numToChange)
+                let newNum = randomNums.randomElement()!
+                let input = number.prefix(replaceCharIndex - 1) + "\(newNum)" + number.dropFirst(replaceCharIndex)
+                cardTextField.textField.secureText = String(input)
+                guard let state = cardTextField.state as? CardState else {
+                    XCTFail("Guard fail")
+                    return
+                }
+                XCTAssert(state.isValid == false, "Not Valid Card number \(number), first digits \(input), for brand \(brand) fail")
+            }
+        }
+    }
+    
+    func testSpecificNotValidCardsValidationReturnsFalse() {
+        let numbers = SwiftLuhn.specificNotValidCardNumbers
+        numbers.forEach { number in
+            cardTextField.textField.secureText = number
+            guard let state = cardTextField.state as? CardState else {
+                XCTFail("Guard fail")
+                return
+            }
+            XCTAssert(state.isValid == false, "Specific Not Valid Card number \(number) validation fail")
+            XCTAssert(state.last4.isEmpty == true, "Specific Not Valid Card number \(number), state.last4.isEmpty == true fail")
+            XCTAssert(state.bin.isEmpty == true, "Specific Not Valid Card number \(number), tate.bin.isEmpty == true fail")
         }
     }
 }
