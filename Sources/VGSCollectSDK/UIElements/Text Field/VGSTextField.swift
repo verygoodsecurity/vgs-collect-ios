7e62d8780a9eab8e0adb0600e7fe7000ce3286c7
Vitalii Obertynskyi <vitalii.obertynskyi@verygoodsecurity.com>
2020-05-26 12:47:17 +0300

    Set default size for icon

--- Sources/VGSCollectSDK/UIElements/Text Field/VGSTextField.swift
+++ Sources/VGSCollectSDK/UIElements/Text Field/VGSTextField.swift
@@ -160,6 +160,7 @@ extension VGSTextField: UITextFieldDelegate {
 // MARK: - private API
 internal extension VGSTextField {
     
+    @objc
     func mainInitialization() {
         // set main style for view
         mainStyle()
