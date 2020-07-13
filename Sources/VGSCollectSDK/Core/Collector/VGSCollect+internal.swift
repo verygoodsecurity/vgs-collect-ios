d284acc618f5ce4e6edcede746d580bfa58fb755
dmytrokhl <57712402+dmytrokhl@users.noreply.github.com>
2020-07-13 09:26:36 +0300

    Live env data regions support (#175)
    * Add multiple live data regions support.

--- Sources/VGSCollectSDK/Core/Collector/VGSCollect+internal.swift
+++ Sources/VGSCollectSDK/Core/Collector/VGSCollect+internal.swift
@@ -27,6 +27,11 @@ internal extension VGSCollect {
         return tenantId.isAlphaNumeric
     }
     
+  /// Validate data region id
+    class func regionValid(_ region: String) -> Bool {
+      return !region.isEmpty && region.range(of: ".*[^a-zA-Z0-9-].*", options: .regularExpression) == nil
+    }
+  
     /// Validate stored textfields input data
     func validateStoredInputData() -> VGSError? {
         return validate(storage.elements)
@@ -102,5 +107,26 @@ internal extension VGSCollect {
         // call observers ONLY after all internal updates done
         observeStates?(storage.elements)
         observeFieldState?(textField)
+    }
+  
+  class func generateVaultURL(tenantId: String, environment: Environment, region: String?) -> URL {
+        
+      var environmentString = environment.rawValue
+    
+      if let region = region, !region.isEmpty {
+        if environment == .live {
+          assert(Self.regionValid(region), "ERROR: DATA REGION IS NOT VALID!!!")
+          environmentString += "-" + region
+        } else {
+          print("NOTE: DATA REGION SHOULD BE USED WITH LIVE ENVIRONMENT ONLY!!!")
+        }
+      }
+      assert(Self.tenantIDValid(tenantId), "ERROR: TENANT ID IS NOT VALID!!!")
+
+      let strUrl = "https://" + tenantId + "." + environmentString + ".verygoodproxy.com"
+      guard let url = URL(string: strUrl) else {
+          fatalError("ERROR: NOT VALID ORGANIZATION PARAMETERS!!!")
+      }
+      return url
     }
 }
