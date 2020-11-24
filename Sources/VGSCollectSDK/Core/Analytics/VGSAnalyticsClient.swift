4b211aace3859e1775b69a5d2789f8592ef60d5c
dmytro.khl <dmytro.khludkov@vgs.io>
2020-11-24 17:48:47 +0200

    Add device details.

--- Sources/VGSCollectSDK/Core/Analytics/VGSAnalyticsClient.swift
+++ Sources/VGSCollectSDK/Core/Analytics/VGSAnalyticsClient.swift
@@ -54,6 +54,7 @@ public class VGSAnalyticsClient {
       return [
               "platform": UIDevice.current.systemName,
               "device": UIDevice.current.model,
+              "deviceModel": UIDevice.current.modelIdentifier,
               "osVersion": osVersion ]
       }()
 
