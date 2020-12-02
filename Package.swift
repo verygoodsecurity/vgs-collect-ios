d756ffb8516c7c4d60929dab0cadf86d750718d6
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2020-12-02 08:22:46 +0200

    Set SPM min toolchain to 5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -23,9 +23,16 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "VGSCollectSDK",
-            dependencies: []),
+  					exclude: [
+							"Info.plist",
+							"VGSCollectSDK.h"
+	 				]),
         .testTarget(
             name: "FrameworkTests",
-            dependencies: ["VGSCollectSDK"]),
-    ]
+            dependencies: ["VGSCollectSDK"],
+						exclude: [
+								"Info.plist"
+						]
+				)
+			]
 )
