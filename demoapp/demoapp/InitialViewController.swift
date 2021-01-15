b4b94b631344d7835ab70f2ab668f71bd1b50014
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-01-15 10:24:52 +0200

    Add fix for iPad

--- demoapp/demoapp/InitialViewController.swift
+++ demoapp/demoapp/InitialViewController.swift
@@ -24,6 +24,13 @@ class InitialViewController: UITableViewController {
       let vaultId = alert.textFields?.first?.text ?? "vaultId"
       AppCollectorConfiguration.shared.vaultId = vaultId
     }))
+
+		if let popoverController = self.popoverPresentationController {
+			popoverController.sourceView = self.view //to set the source of your alert
+			popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0) // you can set this as per your requirement.
+			popoverController.permittedArrowDirections = [] //to hide the arrow of any particular direction
+		}
+
     self.present(alert, animated: true, completion: nil)
   }
 }
