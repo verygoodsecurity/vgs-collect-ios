dc012370ebfa33f277124ce296045fddd6a725b8
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-03-09 08:26:49 +0200

    Add internal doc

--- Sources/VGSCollectSDK/Utils/Helpers/Mappers/VGSFieldNameMapUtils.swift
+++ Sources/VGSCollectSDK/Utils/Helpers/Mappers/VGSFieldNameMapUtils.swift
@@ -13,13 +13,17 @@ internal final class VGSFieldNameMapUtils {
 
 	// MARK: - Private vars
 
-	private static let indexRegexPattern = "\\[\\d+\\]"
+	/// Regex pattern to find array index in format: [1].
+	private static let arrayIndexRegexPattern = "\\[\\d+\\]"
 
 	// MARK: - Public
 
+	/// Map field name to subscripts.
+	/// - Parameter fieldName: `String` object, should be fieldName.
+	/// - Returns: `[VGSFieldNameSubscriptType]` object, array of subscripts.
 	static func mapFieldNameToSubscripts(_ fieldName: String) -> [VGSFieldNameSubscriptType] {
 
-		var subscriptors = [VGSFieldNameSubscriptType]()
+		var subscripts = [VGSFieldNameSubscriptType]()
 
 		// Split key by `.` character.
 		fieldName.split(separator: ".").forEach { component in
@@ -28,11 +32,11 @@ internal final class VGSFieldNameMapUtils {
 			// Try to get normal string key before prefix `[`.
 			if let keyPathPrefix = path.components(separatedBy: "[").first {
 				let subscriptorItem = VGSFieldNameSubscriptType.key(keyPathPrefix)
-				subscriptors.append(subscriptorItem)
+				subscripts.append(subscriptorItem)
 			}
 
 			// Grab all aray indices: [0].
-			let numberPaths = matchesForRegexInText(regex: indexRegexPattern, text: path)
+			let numberPaths = matchesForRegexInText(regex: arrayIndexRegexPattern, text: path)
 
 			// Take only first index.
 			if let first = numberPaths.first {
@@ -44,17 +48,22 @@ internal final class VGSFieldNameMapUtils {
 				if let numInt = Int(num) {
 					print("numInt: \(numInt)")
 					let numberSubs = VGSFieldNameSubscriptType.index(numInt)
-					subscriptors.append(numberSubs)
+					subscripts.append(numberSubs)
 				}
 			}
 		}
 
-		// Return array of subscriptors
-		return subscriptors
+		// Return array of subscripts.
+		return subscripts
 	}
 
 	// MARK: - Private
 
+	/// Find all matches for regex pattern in text. Invalid regex will produce empty array.
+	/// - Parameters:
+	///   - regex: `String` object, valid regex pattern.
+	///   - text: `String` object, text to find matches.
+	/// - Returns: Array of `String` containing all matches.
 	static func matchesForRegexInText(regex: String, text: String) -> [String] {
 
 			do {
