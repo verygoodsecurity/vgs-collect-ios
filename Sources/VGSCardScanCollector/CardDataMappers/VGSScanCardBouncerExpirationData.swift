a551cde530a379c1255e8e45103c50fe56214982
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-04-01 18:43:36 +0300

    Add comments | rename file

--- Sources/VGSCardScanCollector/CardDataMappers/VGSScanCardBouncerExpirationData.swift
+++ Sources/VGSCardScanCollector/CardDataMappers/VGSScanCardBouncerExpirationData.swift
@@ -1,11 +1,12 @@
 //
-//  VGSScanCardExpirationData.swift
+//  VGSScanCardBouncerExpirationData.swift
 //  VGSCardScanCollector
 //
 
 import Foundation
 
 /// Holds scanned expiration date data.
+/// Bouncer holds scanned data in format `0325` -> `03` for month, `25` for year.
 internal struct VGSScanCardBouncerExpirationData {
 	/// Scanned month.
 	internal let monthString: String?
