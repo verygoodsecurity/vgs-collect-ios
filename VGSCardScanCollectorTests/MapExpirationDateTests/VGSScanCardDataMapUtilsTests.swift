83822f1f9cfc94f0c15463cdde477e96c8fdf816
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2021-04-01 17:51:09 +0300

    Move test logic to a separate function

--- VGSCardScanCollectorTests/MapExpirationDateTests/VGSScanCardDataMapUtilsTests.swift
+++ VGSCardScanCollectorTests/MapExpirationDateTests/VGSScanCardDataMapUtilsTests.swift
@@ -11,63 +11,63 @@ import XCTest
 class VGSScanCardDataMapUtilsTests: XCTestCase {
 
 	/// Holds test data.
-	struct TestData {
+	struct TestDataMappingItem {
 		let format: CradScanDataType
 		let expectedText: String
 	}
 
-	/// Tests scanned data for long month.
-	func testScanDataMappingWithLongMonth() {
-		let scannedData = VGSScanCardExpirationData(monthString: "10", yearString: "24")
-
-		let testData: [TestData] = [
-			TestData(format: .expirationDate, expectedText: "1024"),
-
-			TestData(format: .expirationDateLong, expectedText: "102024"),
-
-			TestData(format: .expirationMonth, expectedText: "10"),
-
-			TestData(format: .expirationYear, expectedText: "24"),
-
-			TestData(format: .expirationYearLong, expectedText: "2024")
-		]
-
-		for index in 0..<testData.count {
-			let item = testData[index]
-			let format = item.format
-			let expectedText = item.expectedText
-
-			let outputText = "Index: \(index). \nCannot convert text for data: month: 10, year: 24. \nFormat: \(format). \nExpectedText: \(expectedText)"
-			if let text = VGSScanCardDataMapUtils.mapCardExpirationData(scannedData, scannedDataType: format) {
-				XCTAssertTrue(text == expectedText, outputText + "actual result: \(text)")
-			} else {
-				XCTFail(outputText + "actual result: *nil*")
-			}
-		}
+	struct TestDataItem {
+		let items: [TestDataMappingItem]
+		let scannedData: VGSScanCardExpirationData
 	}
 
-	/// Tests scanned data for short month.
-	func testScanDataMappingWithShortMonth() {
-		let scannedData = VGSScanCardExpirationData(monthString: "03", yearString: "25")
+	private let testDataItemLongMonth = TestDataItem(items: [
+																							TestDataMappingItem(format: .expirationDate, expectedText: "1024"),
 
-		let testData: [TestData] = [
-			TestData(format: .expirationDate, expectedText: "0325"),
+																							TestDataMappingItem(format: .expirationDateLong, expectedText: "102024"),
 
-			TestData(format: .expirationDateLong, expectedText: "032025"),
+																							TestDataMappingItem(format: .expirationMonth, expectedText: "10"),
 
-			TestData(format: .expirationMonth, expectedText: "03"),
+																							TestDataMappingItem(format: .expirationYear, expectedText: "24"),
 
-			TestData(format: .expirationYear, expectedText: "25"),
+																							TestDataMappingItem(format: .expirationYearLong, expectedText: "2024")],
+																						scannedData: VGSScanCardExpirationData(monthString: "10", yearString: "24"))
 
-			TestData(format: .expirationYearLong, expectedText: "2025")
-		]
+	private let testDataItemShortMonth = TestDataItem(items: [
+																							TestDataMappingItem(format: .expirationDate, expectedText: "0325"),
+
+																							TestDataMappingItem(format: .expirationDateLong, expectedText: "032025"),
+
+																							TestDataMappingItem(format: .expirationMonth, expectedText: "03"),
+
+																							TestDataMappingItem(format: .expirationYear, expectedText: "25"),
+
+																							TestDataMappingItem(format: .expirationYearLong, expectedText: "2025")],
+																						scannedData: VGSScanCardExpirationData(monthString: "03", yearString: "25"))
+
+	/// Test scan data.
+	func testScannedData() {
+		verifyScanDataItem(testDataItemShortMonth)
+		verifyScanDataItem(testDataItemLongMonth)
+	}
+
+	// MARK: - Helpers
+
+	/// Verify scanned data mapping.
+	/// - Parameter testDataItem: `TestDataItem` object, holds test data.
+	private func verifyScanDataItem(_ testDataItem: TestDataItem) {
+		let scannedData = testDataItem.scannedData
+		let testData = testDataItem.items
 
 		for index in 0..<testData.count {
 			let item = testData[index]
 			let format = item.format
 			let expectedText = item.expectedText
 
-			let outputText = "Index: \(index). \nCannot convert text for data: month: 03, year: 25. \nFormat: \(format). \nExpectedText: \(expectedText)"
+			let scannedMonth = scannedData.monthString ?? ""
+			let scannedYear = scannedData.yearString ?? ""
+
+			let outputText = "Index: \(index). \nCannot convert text for data: month: \(scannedMonth), year: \(scannedYear). \nFormat: \(format). \nExpectedText: \(expectedText)"
 			if let text = VGSScanCardDataMapUtils.mapCardExpirationData(scannedData, scannedDataType: format) {
 				XCTAssertTrue(text == expectedText, outputText + "actual result: \(text)")
 			} else {
