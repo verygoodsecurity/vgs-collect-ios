fa97a343cb6935c55f8d038345022c5d6c442ef7
dmytro.khl <dmytro.khludkov@vgs.io>
2021-04-12 15:15:17 +0300

    Update default valid brands behaviour.

--- Sources/VGSCollectSDK/Core/PaymentCards/VGSPaymentCards.swift
+++ Sources/VGSCollectSDK/Core/PaymentCards/VGSPaymentCards.swift
@@ -92,9 +92,9 @@ public class VGSPaymentCards {
     /// - Note: the order have impact on which card brand should be detected first by `VGSPaymentCardModel.regex`.
     public static var cutomPaymentCardModels = [VGSCustomPaymentCardModel]()
 
-    /// An array of valid Card Brands, could include custom and default brands.
+    /// An array of valid Card Brands, could include custom and default brands. If not set, will use `availableCardBrands` array insted.
     /// - Note: the order have impact on which card brand should be detected first by `VGSPaymentCardModel.regex`.
-    public static var validCardBrands: [VGSPaymentCardModelProtocol] = availableCards
+    public static var validCardBrands: [VGSPaymentCardModelProtocol]?
 
     /// Array of Available Cards.
     /// -  Note: the order have impact on which card brand should be detected first by `VGSPaymentCardModel.regex`.
@@ -113,8 +113,16 @@ public class VGSPaymentCards {
                                                       unionpay,
                                                       jcb ] }
       
-    /// Array of All  Card Models(Custom + Default). Note: the order have impact on which card brand should be detected first by  `VGSPaymentCardModel.regex`
-     internal static var availableCards: [VGSPaymentCardModelProtocol] {
+    /// Array of CardBrands that should be supported by SDK.
+    ///  Will return an array of `validCardBrands` when it's not nil.
+    ///  Will return All Card Models(Custom + Default) if specific `validCardBrands` is nil.
+    /// - Note: the order have impact on which card brand should be detected first by  `VGSPaymentCardModel.regex`
+     internal static var availableCardBrands: [VGSPaymentCardModelProtocol] {
+      /// Check if uset setup an array of specific CardBrands that should be supported by SDK.
+      if let customBrandSet = validCardBrands {
+        return customBrandSet
+      }
+      /// If no specific valid brands, return All Card Models(Custom + Default)
       return Self.cutomPaymentCardModels + Self.defaultCardModels
     }
 }
@@ -160,11 +168,11 @@ public extension VGSPaymentCards.CardBrand {
 internal extension VGSPaymentCards {
     
     static func getCardModelFromAvailableModels(brand: VGSPaymentCards.CardBrand) -> VGSPaymentCardModelProtocol? {
-      return Self.validCardBrands.first(where: { $0.brand == brand})
+      return Self.availableCardBrands.first(where: { $0.brand == brand})
     }
 
     static func detectCardBrandFromAvailableCards(input: String) -> VGSPaymentCards.CardBrand {
-      for cardModel in Self.validCardBrands {
+      for cardModel in Self.availableCardBrands {
           let predicate = NSPredicate(format: "SELF MATCHES %@", cardModel.regex)
           if predicate.evaluate(with: input) == true {
             return cardModel.brand
