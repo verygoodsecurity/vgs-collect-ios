6f0c6afb938e7993b97dbc74294622248b6bcb5a
Dylan Bettermann <dylan.bettermann@doordash.com>
2021-06-08 16:18:08 -0500

    Add Info.plist to exclude

--- Package.swift
+++ Package.swift
@@ -39,7 +39,8 @@ let package = Package(
         .target(
             name: "VGSCollectSDK",
   					exclude: [
-							"VGSCollectSDK.h"
+							"VGSCollectSDK.h",
+							"Info.plist"
 	 				]),
         .testTarget(
             name: "FrameworkTests",
