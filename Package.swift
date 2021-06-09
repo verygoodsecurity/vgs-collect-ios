451d3c031a0f051dbca9e65235d2acfb16e78f6d
dmytro.khl <dmytro.khludkov@vgs.io>
2021-06-09 21:19:59 +0300

    Update Swift Package config.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "VGSCollectSDK",
-            targets: ["VGSCollectSDK, VGSPaymentCards"]),
+            targets: ["VGSCollectSDK", "VGSPaymentCards"]),
 				.library(
 						name: "VGSCardScanCollector",
 						targets: ["VGSCardScanCollector"]
@@ -41,6 +41,12 @@ let package = Package(
   					exclude: [
 							"VGSCollectSDK.h"
 	 				]),
+        .target(name: "VGSPaymentCards",
+                path: "Sources/ VGSPaymentCards/",
+                exclude:  [
+                  "VGSPaymentCards.h"
+                ]
+        ),
         .testTarget(
             name: "FrameworkTests",
             dependencies: ["VGSCollectSDK"]
