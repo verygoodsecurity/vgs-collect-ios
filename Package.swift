a33ccea29cc5d977e5206a0bf6a1cb3f1f190bcf
dmytro.khl <dmytro.khludkov@vgs.io>
2021-06-09 20:43:16 +0300

    Update Swift Package.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "VGSCollectSDK",
-            targets: ["VGSCollectSDK"]),
+            targets: ["VGSCollectSDK, VGSPaymentCards"]),
 				.library(
 						name: "VGSCardScanCollector",
 						targets: ["VGSCardScanCollector"]
