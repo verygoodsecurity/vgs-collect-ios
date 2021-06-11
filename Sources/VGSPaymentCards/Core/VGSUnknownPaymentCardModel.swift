dda910d1ff2ed6b69c29f3fd5b2335b0dc60ce23
dmytro.khl <dmytro.khludkov@vgs.io>
2021-06-11 15:52:58 +0300

    Update access to atributes.

--- Sources/VGSPaymentCards/Core/VGSUnknownPaymentCardModel.swift
+++ Sources/VGSPaymentCards/Core/VGSUnknownPaymentCardModel.swift
@@ -16,8 +16,8 @@ public struct VGSUnknownPaymentCardModel {
   
   internal let name = VGSPaymentCards.CardBrand.unknown.defaultName
   
-  /// TODO: Change to internal
-  public var regex: String = VGSPaymentCards.CardBrand.unknown.defaultRegex
+  /// Regex validating that input contains digits only.
+  public let regex: String = VGSPaymentCards.CardBrand.unknown.defaultRegex
 
   /// Valid Unknown Card Numbers Lengths
   public var cardNumberLengths: [Int] = VGSPaymentCards.CardBrand.unknown.defaultCardLengths
