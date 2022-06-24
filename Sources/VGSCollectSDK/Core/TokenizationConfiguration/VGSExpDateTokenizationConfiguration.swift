a093caf421a608ca1b2293ca98edc10b33669813
Dmytro Khludkov <dmytro.khludkov@vgs.io>
2022-06-24 19:31:01 +0300

    Update exp_date tokenization configuration.

--- Sources/VGSCollectSDK/Core/TokenizationConfiguration/VGSExpDateTokenizationConfiguration.swift
+++ Sources/VGSCollectSDK/Core/TokenizationConfiguration/VGSExpDateTokenizationConfiguration.swift
@@ -15,17 +15,29 @@ public struct VGSExpDateTokenizationParameters: VGSTokenizationParametersProtoco
 }
 
 /// `VGSExpDateTokenizationConfiguration` - textfield configuration for textfield with type `.expDate`, required for work with tokenization api.
-public class VGSExpDateTokenizationConfiguration: VGSConfiguration, VGSTextFieldTokenizationConfigurationProtocol {
+public final class VGSExpDateTokenizationConfiguration: VGSConfiguration, VGSExpDateConfigurationProtocol, VGSTextFieldTokenizationConfigurationProtocol {
   
-  /// `VGSExpDateTokenizationParameters` - tokenization configuration parameters.
-  public var tokenizationPatameters = VGSExpDateTokenizationParameters()
-
+  // MARK: - Attributes
   /// `FieldType.expDate` type of `VGSTextField`tokenization  configuration.
   override public var type: FieldType {
     get { return .expDate }
     set {}
   }
   
+  // MARK: - VGSExpDateConfigurationProtocol
+  /// Input Source type. Default is `VGSTextFieldInputSource.datePicker`.
+  public var inputSource: VGSTextFieldInputSource = .datePicker
+  
+  /// Input date format to convert.
+  public var inputDateFormat: VGSCardExpDateFormat?
+  
+  /// Output date format.
+  public var outputDateFormat: VGSCardExpDateFormat?
+  
+  // MARK: - VGSTokenizationParametersProtocol
+  /// `VGSExpDateTokenizationParameters` - tokenization configuration parameters.
+  public var tokenizationPatameters = VGSExpDateTokenizationParameters()
+
   internal var tokenizationConfiguration: VGSTokenizationParametersProtocol {
     return tokenizationPatameters
   }
