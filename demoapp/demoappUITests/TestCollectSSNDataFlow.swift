f7015d34d58f3fd3908c5c1b03b4611857fe17b3
Eugene Goloboyar <ievgen.goloboiar@vgs.io>
2022-11-12 15:29:38 +0200

    Refactor ssn flow

--- demoapp/demoappUITests/TestCollectSSNDataFlow.swift
+++ demoapp/demoappUITests/TestCollectSSNDataFlow.swift
@@ -8,23 +8,72 @@
 
 import XCTest
 
+/// Tests for SSN data flow.
 class TestCollectSSNDataFlow: TestCollectBaseTestCase {
 
-     func testPutCorrectData() {
-        app.tables.staticTexts["Collect Social Security Number"].tap()
+  /// UI elements.
+  enum UIElements {
 
-        let ssnField = app.textFields["XXX-XX-XXXX"]
-        ssnField.tap()
-        ssnField.typeText("111558899")
+    /// VGSText field.
+    enum VGSTextField {
+
+      /// SSN fields.
+      enum SSN {
+        /// Card holder name.
+        static let ssn: VGSUITestElement = .init(type: .textField, identifier: "XXX-XX-XXXX")
+      }
+    }
+
+    /// Labels.
+    enum Labels {
+
+      /// Console label identifier.
+      static let consoleLabelIdentifier = "ConsoleLabelIdentifire"
+
+      /// State label.
+      static let state = "STATE"
+
+      /// Response label.
+      static let response = "RESPONSE"
+    }
+
+    /// Buttons.
+    enum Buttons {
+
+      /// Upload.
+      static let upload: VGSUITestElement = .init(type: .button, identifier: "UPLOAD")
+    }
+
+    /// Texts.
+    enum Texts {
+
+      /// Raw ssn.
+      static let rawSSN = "111558899"
+
+      /// Masked ssn.
+      static let maskedSSN = "SSN: XXX-XX-8899"
+    }
+  }
+
+  /// Verifies correct data flow works for ssn.
+  func testPutCorrectData() {
+    // Navigate to SSN.
+    app.tables.staticTexts[TestsCollectFlowType.ssn.name].tap()
+
+    let ssnField = UIElements.VGSTextField.SSN.ssn.find(in: app)
+    ssnField.tap()
+    ssnField.typeText(UIElements.Texts.rawSSN)
     
-        app.staticTexts["SSN: XXX-XX-8899"].tap()
-        app.buttons["UPLOAD"].tap()
-        
-        let responseLabel = app.staticTexts["RESPONSE"]
-        responseLabel.waitForExistence(timeout: 30)
-        
-        let successResponsePredicate = NSPredicate(format: "label BEGINSWITH 'Success: '")
-        let successResponseLabel = app.staticTexts.element(matching: successResponsePredicate)
-        XCTAssert(successResponseLabel.exists)
+    app.staticTexts[UIElements.Texts.maskedSSN].tap()
+    UIElements.Buttons.upload.find(in: app).tap()
+
+    // Wait for request.
+    wait(forTimeInterval: 30)
+
+    // Find response label.
+    let responseLabel = app.staticTexts[UIElements.Labels.response]
+
+    // Verify success response.
+    verifySuccessResponse()
   }
 }
