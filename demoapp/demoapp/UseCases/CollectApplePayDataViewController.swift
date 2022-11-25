6837d9f9c1cd8385871da26c48bd4ad9fce85290
Dmytro Khludkov <dmytro.khludkov@vgs.io>
2022-11-25 16:49:20 +0200

    Update demo app.

--- demoapp/demoapp/UseCases/CollectApplePayDataViewController.swift
+++ demoapp/demoapp/UseCases/CollectApplePayDataViewController.swift
@@ -3,17 +3,33 @@
 //  demoapp
 //
 
+/**
+ Below is code example how to collect and decrypt ApplePay payment requests with VGS.
+ This require few steps:
+ 1. Create  certificate signing request (CSR file) and generate Payment Processing Certificate at Apple Dashboard.
+ 2. Configure VGS Route with ApplePay Merchant ID, Vault Access Credentials, Payment Processing Certificate.
+ 3. Get payment data from `PKPayment.token` and send data to your vault with VGSCollect.send() request.
+ 
+ You can find more details how to prepare VGS Route configuration to handle ApplePay here -  https://github.com/vgs-samples/apple-pay-setup .
+ 
+ NOTE: This use case can be tested only on real iOS device!
+ **/
+
 import Foundation
 import PassKit
 import VGSCollectSDK
-
+                                                  
 class CollectApplePayData: UIViewController {
-  @IBOutlet weak var buttonsStackView: UIStackView!
   
-  var collector = VGSCollect(id: "tnt", environment: "sandbox")
+  @IBOutlet weak var payButtonsStackView: UIStackView!
+  @IBOutlet weak var consoleLabel: UILabel!
+  @IBOutlet weak var consoleStatusLabel: UILabel!
   
+  /// Init VGS Collect
+  var collector = VGSCollect(id: AppCollectorConfiguration.shared.vaultId, environment: AppCollectorConfiguration.shared.environment)
+  /// ApplePay payment controller
   var paymentController: PKPaymentAuthorizationController?
-  
+  /// Supported Networks via ApplePay
   static let supportedNetworks: [PKPaymentNetwork] = [
       .amex,
       .discover,
@@ -21,94 +37,102 @@ class CollectApplePayData: UIViewController {
       .visa
   ]
 
-  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     VGSCollectLogger.shared.configuration.isNetworkDebugEnabled = true
     VGSCollectLogger.shared.configuration.level = .info
     
-    /// Check if Apple Pay available
+    /// Check if ApplePay option available
     let result = Self.applePayStatus()
-    if result.canMakePayments {
-        let applePayButton = PKPaymentButton(paymentButtonType: .checkout, paymentButtonStyle: .black)
-        applePayButton.addTarget(self, action: #selector(Self.payPressed), for: .touchUpInside)
-          let constraints = [
-              applePayButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-              applePayButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-          ]
-          applePayButton.translatesAutoresizingMaskIntoConstraints = false
-          view.addSubview(applePayButton)
-          NSLayoutConstraint.activate(constraints)
-      }
+    if result.canMakePayments && result.canSetupCards {
+      let applePayButton = PKPaymentButton(paymentButtonType: .checkout, paymentButtonStyle: .black)
+      applePayButton.addTarget(self, action: #selector(Self.payPressed), for: .touchUpInside)
+      payButtonsStackView.addArrangedSubview(applePayButton)
     }
+  }
 
+  /// Get ApplePay status
   class func applePayStatus() -> (canMakePayments: Bool, canSetupCards: Bool) {
       return (PKPaymentAuthorizationController.canMakePayments(),
               PKPaymentAuthorizationController.canMakePayments(usingNetworks: supportedNetworks))
   }
   
-  var controller: PKPaymentAuthorizationViewController?
-
+  /// Setup payment request
   private var paymentRequest: PKPaymentRequest = {
-    let ticket = PKPaymentSummaryItem(label: "Shopping Items", amount: NSDecimalNumber(string: "50.97"), type: .final)
+    let items = PKPaymentSummaryItem(label: "Shopping Items", amount: NSDecimalNumber(string: "50.97"), type: .final)
     let tax = PKPaymentSummaryItem(label: "Tax", amount: NSDecimalNumber(string: "5.00"), type: .final)
     let total = PKPaymentSummaryItem(label: "Total", amount: NSDecimalNumber(string: "50.97"), type: .final)
-    let paymentSummaryItems = [ticket, tax, total]
+    let paymentSummaryItems = [items, tax, total]
     
     let paymentRequest = PKPaymentRequest()
     paymentRequest.paymentSummaryItems = paymentSummaryItems
-    paymentRequest.merchantIdentifier = "<merchantid>"
+    /// Merchand id associated with ApplePay Payment Processing Certificate.
+    paymentRequest.merchantIdentifier = "merchant.com.vgs-checkout-ios-test"
     paymentRequest.merchantCapabilities = .capability3DS
     paymentRequest.countryCode = "US"
     paymentRequest.currencyCode = "USD"
     paymentRequest.supportedNetworks = supportedNetworks
-    paymentRequest.shippingType = .delivery
-    paymentRequest.requiredShippingContactFields = [.name, .postalAddress]
     return paymentRequest
   }()
   
+  /// Handle ApplePay button pressed
+  @objc func payPressed(sender: AnyObject) {
+      self.startPayment()
+  }
   
+  /// Initiate PKPayment Authorization request
   func startPayment() {
+    self.consoleStatusLabel.text = "Collecting ApplePay data"
     paymentController = PKPaymentAuthorizationController(paymentRequest: paymentRequest)
     paymentController?.delegate = self
     paymentController?.present(completion: { (presented: Bool) in
-        if presented {
-            debugPrint("Presented payment controller")
-        } else {
-            debugPrint("Failed to present payment controller")
-//            self.completionHandler(false)
+        if !presented {
+          self.consoleLabel.text = "Failed to present payment controller"
         }
     })
   }
-
-    @objc func payPressed(sender: AnyObject) {
-        self.startPayment()
-    }
 }
 
+/// Handle PKPaymentAuthorizationController results
 extension CollectApplePayData: PKPaymentAuthorizationControllerDelegate {
-  func paymentAuthorizationControllerDidFinish(_ controller: PKPaymentAuthorizationController) {
-    controller.dismiss(completion: nil)
-  }
-  
- 
+
   func paymentAuthorizationController(_ controller: PKPaymentAuthorizationController, didAuthorizePayment payment: PKPayment, handler completion: @escaping (PKPaymentAuthorizationResult) -> Void) {
-    
-      let paymentDataDictionary: [String: Any] = try! JSONSerialization.jsonObject(with: payment.token.paymentData, options: .mutableContainers) as! [String : Any]
+      /// Prepapre payment data required to collect for your use-case
+    guard let paymentDataDictionary: [String: Any] = try? JSONSerialization.jsonObject(with: payment.token.paymentData, options: .mutableContainers) as? [String : Any] else {
+      print("Error: can't parse payment.token.paymentData")
+      completion(PKPaymentAuthorizationResult(status: .failure, errors: nil))
+      return
+    }
       let paymentData = ["paymentData": paymentDataDictionary]
       let paymentToken = ["token": paymentData]
     
       /// Send data to your vault
-      collector.sendData(path: "/post", extraData: paymentToken) { response in
-        switch response {
-        case .success(let _, let _, let response):
-          print(response)
+    collector.sendData(path: "/post", extraData: paymentToken) { [weak self] result in
+        self?.consoleStatusLabel.text = "RESPONSE"
+
+        switch result {
+        case .success(let code, let data, _):
+          print("code: \(code)")
+          if let data = data, let jsonData = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+            // swiftlint:disable force_try
+            let response = (String(data: try! JSONSerialization.data(withJSONObject: jsonData["json"]!, options: .prettyPrinted), encoding: .utf8)!)
+            self?.consoleLabel.text = "Success: \n\(response)"
+            print(response)
+            // swiftlint:enable force_try
+            }
+            return
+          
           completion(PKPaymentAuthorizationResult(status: .success, errors: nil))
-        case .failure(let _, let _, let _, let _):
+        case .failure(let code, _, _, _):
+          self?.consoleLabel.text = "Error: Something went wrong. Code: \(code)"
           completion(PKPaymentAuthorizationResult(status: .failure, errors: nil))
         }
       }
     }
+  
+  func paymentAuthorizationControllerDidFinish(_ controller: PKPaymentAuthorizationController) {
+    controller.dismiss(completion: nil)
+  }
 }
 
