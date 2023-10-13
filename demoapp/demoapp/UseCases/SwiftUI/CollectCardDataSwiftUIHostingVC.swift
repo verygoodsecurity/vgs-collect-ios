bc36d322087ad3da3c57399bc8719c4b5bc82bab
Dmytro Khludkov <dmytro.khludkov@vgs.io>
2023-10-13 16:52:26 +0300

    SwiftUI-UIKit bridge.

---
+++ demoapp/demoapp/UseCases/SwiftUI/CollectCardDataSwiftUIHostingVC.swift
@@ -0,0 +1,30 @@
+//
+//  CollectCardDataSwiftUIHostingVC.swift
+//  demoapp
+//
+
+
+import Foundation
+import SwiftUI
+
+class CollectCardDataSwiftUIHostingVC: UIViewController {
+  override func viewDidLoad() {
+          super.viewDidLoad()
+
+          let swiftUIView = CollectCardDataSwiftUI()
+          let hostingController = UIHostingController(rootView: swiftUIView)
+          addChild(hostingController)
+          hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+          view.addSubview(hostingController.view)
+
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+
+      hostingController.didMove(toParent: self)
+  }
+}
+
