9e89db8195f97089c101ac166b0f2e84d581badf
Dmytro Khludkov <dmytro.khludkov@vgs.io>
2023-10-13 16:48:57 +0300

    SwiftUI demo app.

---
+++ VGSCollectSwiftUIDemoApp/VGSCollectSwiftUIDemoApp/VGSCollectSwiftUIDemoAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  VGSCollectSwiftUIDemoAppApp.swift
+//  VGSCollectSwiftUIDemoApp
+//
+//  Created by Dmytro on 18.09.2023.
+//
+
+import SwiftUI
+import VGSCollectSDK
+
+@main
+struct VGSCollectSwiftUIDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
