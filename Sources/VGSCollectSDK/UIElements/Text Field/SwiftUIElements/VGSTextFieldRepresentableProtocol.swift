4b45aee3a3a70aaddba04c213c7c02a0327f8fca
Dmytro Khludkov <dmytro.khludkov@vgs.io>
2024-01-22 16:10:36 +0200

    Add //  VGSTextFieldRepresentableProtocol and VGSCardTextFieldRepresentableProtocol.

---
+++ Sources/VGSCollectSDK/UIElements/Text Field/SwiftUIElements/VGSTextFieldRepresentableProtocol.swift
@@ -0,0 +1,71 @@
+//
+//  VGSTextFieldRepresentableProtocol.swift
+//  VGSCollectSDK
+//
+
+import Foundation
+
+internal protocol VGSTextFieldRepresentableProtocol {
+  
+  /// Textfiled text font.
+  var font: UIFont? {get set}
+  /// Placeholder string.
+  var placeholder: String? {get set}
+  /// Textfield attributedPlaceholder string.
+  var attributedPlaceholder: NSAttributedString? {get set}
+  /// Textfield autocapitalization type.
+  var autocapitalizationType: UITextAutocapitalizationType {get set}
+  /// Determines whether autocorrection is enabled or disabled during typing.
+  var autocorrectionType: UITextAutocorrectionType {get set}
+  /// Textfield spell checking type.
+  var spellCheckingType: UITextSpellCheckingType {get set}
+   /// `UIEdgeInsets` for text and placeholder inside `VGSTextField`.
+  var padding: UIEdgeInsets  {get set}
+  /// The technique to use for aligning the text.
+  var textAlignment: NSTextAlignment {get set}
+  /// Sets when the clear button shows up.
+  var clearButtonMode: UITextField.ViewMode {get set}
+  /// Identifies whether the text object should disable text copying and in some cases hide the text being entered. Default is false.
+  var isSecureTextEntry: Bool {get set}
+  /// Indicates whether textfiled  should automatically update its font when the device’s `UIContentSizeCategory` is changed.
+  var adjustsFontForContentSizeCategory: Bool {get set}
+  /// Input Accessory View
+  var keyboardAccessoryView: UIView? {get set}
+  /// Field border color.
+  var borderColor: UIColor? {get set}
+  /// Field border line width.
+  var bodrerWidth: CGFloat? {get set}
+
+  // MARK: - Accessibility Attributes
+  /// A succinct label in a localized string that identifies the accessibility text field.
+  var textFieldAccessibilityLabel: String? {get set}
+}
+
+/// A base set of optional methods to manage editing text in a text field object.
+internal protocol VGSTextFieldDelegateRepresentableProtocol {
+  /// TextField  did become first responder.
+  var onEditingStart: (() -> Void)? {get set}
+  /// TextField input changed.
+  var onCharacterChange: (() -> Void)? {get set}
+  /// TextField did resign first responder.
+  var onEditingEnd: (() -> Void)? {get set}
+}
+
+/// A  set of optional methods to manage editing text and validation in a text field object.
+internal protocol VGSTextFieldEditingRepresentableProtocol: VGSTextFieldDelegateRepresentableProtocol {
+  /// Returns new `VGSTextFieldState` object on change.
+  var onStateChange: ((VGSTextFieldState) -> Void)? {get set}
+}
+
+/// A  set of optional methods to manage editing text and validation in a card  text field object.
+internal protocol VGSCardTextFieldEditingRepresentableProtocol: VGSTextFieldDelegateRepresentableProtocol {
+  /// Returns new `VGSTextFieldState` object on change.
+  var onStateChange: ((CardState) -> Void)? {get set}
+}
+
+internal protocol VGSCardTextFieldRepresentableProtocol: VGSTextFieldRepresentableProtocol {
+  /// Card brand icon size.
+  var cardIconSize: CGSize {get set}
+  /// Card brand icon positions enum.
+  var cardIconLocation: VGSCardTextField.CardIconLocation {get set}
+}
