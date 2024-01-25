b652994137192d46059d4e813b4423b8805492f4
Dmytro Khludkov <dmytro.khludkov@vgs.io>
2024-01-25 16:12:00 +0200

    Update demo.

--- demoapp/demoapp/UseCases/SwiftUI/CardDataCollectionSwiftUI.swift
+++ demoapp/demoapp/UseCases/SwiftUI/CardDataCollectionSwiftUI.swift
@@ -2,71 +2,133 @@
 //  CardDataCollectionSwiftUI.swift
 //  demoapp
 //
-//  Created by Dmytro on 12.01.2024.
-//  Copyright © 2024 Very Good Security. All rights reserved.
-//
 
 import SwiftUI
 import VGSCollectSDK
 
 struct CardDataCollectionSwiftUI: View {
-    let vgsCollect = VGSCollect(id: "tnt")
-    @State private var cardTextFieldState: VGSTextFieldState? = nil
+    let vgsCollect = VGSCollect(id: AppCollectorConfiguration.shared.vaultId, environment: AppCollectorConfiguration.shared.environment)
   
+    // MARK: - State
+    @State private var holderTextFieldState: VGSTextFieldState? = nil
+    @State private var cardTextFieldState: VGSCardState? = nil
+    @State private var expDateTextFieldState: VGSTextFieldState? = nil
+    @State private var cvcTextFieldState: VGSTextFieldState? = nil
+
+    // MARK: - Textfield UI attributes
     let paddings = UIEdgeInsets(top: 2,left: 8,bottom: 2,right: 8)
-    var borderColor: UIColor {
-      cardTextFieldState?.isValid ?? true ? .lightGray : .red
-    }
-  
-    var cardNumConfiguration: VGSConfiguration {
-      let config = VGSConfiguration(collector: vgsCollect, fieldName: "cardNumber")
-      config.type = .cardNumber
-      return config
-    }
-  
-    var expDateConfiguration: VGSConfiguration {
-      let config = VGSConfiguration(collector: vgsCollect, fieldName: "expDate")
-      config.type = .expDate
-      return config
-    }
-
+    let validColor = UIColor.lightGray
+    let invalidColor = UIColor.red
+        
+    // MARK: - Build View
     var body: some View {
-      return VStack(spacing: 8) {
-      VGSCardTextFieldRepresentable(configuration: cardNumConfiguration)
-        .placeholder("4111 1111 1111 1111")
-        .onStateChange { newState in
-          cardTextFieldState = newState
-          print(newState.isValid)
-        }
-          .cardIconSize(CGSize(width: 40, height: 20))
-        .cardIconLocation(.right)
-        .padding(paddings)
-        .border(color: borderColor, lineWidth: 1)
-        .frame(height: 50)
-        .padding()
-      VGSTextFieldRepresentable(configuration: expDateConfiguration)
-        .onEditingEnd{
-          print("onEditingEnd")
-        }
-        .onEditingStart {
-          print("onEditingStart")
-
-        }
-        .padding(paddings)
-        .placeholder("10/25")
-        .frame(height: 34)
-        .padding()
       
-      Button(action: sendData) {
-        Text("Submit")
+      // MARK: - Textfield Configuration
+      var holderNameConfiguration: VGSConfiguration {
+        let config = VGSConfiguration(collector: vgsCollect, fieldName: "cardHolder_name")
+        config.type = .cardHolderName
+        return config
       }
-    }
+      
+      var cardNumConfiguration: VGSConfiguration {
+        let config = VGSConfiguration(collector: vgsCollect, fieldName: "card_number")
+        config.type = .cardNumber
+        return config
+      }
+      
+      var expDateConfiguration: VGSExpDateConfiguration {
+        let config = VGSExpDateConfiguration(collector: vgsCollect, fieldName: "card_expirationDate")
+        config.type = .expDate
+        return config
+      }
+      var cvcConfiguration: VGSConfiguration {
+        let config = VGSConfiguration(collector: vgsCollect, fieldName: "card_cvc")
+        config.type = .cvc
+        return config
+      }
+      
+      return VStack(spacing: 8) {
+        VGSTextFieldRepresentable(configuration: holderNameConfiguration)
+          .placeholder("Cardholder name")
+          .onEditingStart {
+            print("- Cardholder name onEditingStart")
+          }
+          .onEditingEnd{
+            print("- Cardholder name onEditingEnd")
+          }
+          .textFieldPadding(paddings)
+          .border(color: (holderTextFieldState?.isValid ?? true) ? validColor : invalidColor, lineWidth: 1)
+          .frame(height: 54)
+        VGSCardTextFieldRepresentable(configuration: cardNumConfiguration)
+          .placeholder("4111 1111 1111 1111")
+          .onStateChange { newState in
+            cardTextFieldState = newState
+            print(newState.isValid)
+          }
+          .cardIconSize(CGSize(width: 40, height: 20))
+          .cardIconLocation(.right)
+          .textFieldPadding(paddings)
+          .border(color: (cardTextFieldState?.isValid ?? true) ? validColor : invalidColor, lineWidth: 1)
+          .frame(height: 54)
+        HStack(spacing: 20) {
+          VGSExpDateTextFieldRepresentable(configuration: expDateConfiguration)
+            .placeholder("10/25")
+            .textFieldPadding(paddings)
+            .border(color: (expDateTextFieldState?.isValid ?? true) ? validColor : invalidColor, lineWidth: 1)
+            .frame(height: 54)
+          VGSCVCTextFieldRepresentable(configuration: cvcConfiguration)
+            .placeholder("CVC/CVV")
+            .setSecureTextEntry(true)
+            .cvcIconSize(CGSize(width: 30, height: 20))
+            .textFieldPadding(paddings)
+            .border(color: (cvcTextFieldState?.isValid ?? true) ? validColor : invalidColor, lineWidth: 1)
+            .frame(height: 54)
+        }
+        Button(action: { sendData()}) {
+            Text("Send data")
+                .padding()
+                .cornerRadius(8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.blue, lineWidth: 2)
+                )
+        }.padding(.top, 50)
+      }.padding(.leading, 20)
+       .padding(.trailing, 20)
   }
   
   private func sendData() {
-    print(vgsCollect.textFields)
-    vgsCollect.sendData(path: "post") { response in
-      print(response)
+    /// send extra data
+    var extraData = [String: Any]()
+    extraData["customKey"] = "Custom Value"
+
+    /// Send Data from VGS textfield to Vault
+    vgsCollect.sendData(path: "/post", extraData: extraData) { (response) in
+      
+      switch response {
+      case .success(_, let data, _):
+        if let data = data, let jsonData = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+          // swiftlint:disable force_try
+          let response = (String(data: try! JSONSerialization.data(withJSONObject: jsonData["json"]!, options: .prettyPrinted), encoding: .utf8)!)
+          print("Success: \n\(response)")
+          // swiftlint:enable force_try
+          }
+          return
+      case .failure(let code, _, _, let error):
+        switch code {
+        case 400..<499:
+          // Wrong request. This also can happend when your Routs not setup yet or your <vaultId> is wrong
+          print("Error: Wrong Request, code: \(code)")
+        case VGSErrorType.inputDataIsNotValid.rawValue:
+          if let error = error as? VGSError {
+            print("Error: Input data is not valid. Details:\n \(error)")
+          }
+        default:
+                    print("Error: Something went wrong. Code: \(code)")
+        }
+        print("Submit request error: \(code), \(String(describing: error))")
+        return
+      }
     }
   }
 }
--- demoapp/demoapp/UseCases/SwiftUI/CollectCardDataSwiftUIHostingVC.swift
+++ demoapp/demoapp/UseCases/SwiftUI/CollectCardDataSwiftUIHostingVC.swift
@@ -9,22 +9,23 @@ import SwiftUI
 
 class CollectCardDataSwiftUIHostingVC: UIViewController {
   override func viewDidLoad() {
-          super.viewDidLoad()
+    super.viewDidLoad()
 
-          let swiftUIView = CardDataCollectionSwiftUI()
-          let hostingController = UIHostingController(rootView: swiftUIView)
-          addChild(hostingController)
-          hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-          view.addSubview(hostingController.view)
+    let swiftUIView = CardDataCollectionSwiftUI()
+    let hostingController = UIHostingController(rootView: swiftUIView)
+    addChild(hostingController)
+    hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(hostingController.view)
 
-        NSLayoutConstraint.activate([
-            hostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-        ])
+      NSLayoutConstraint.activate([
+          hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+          hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+          hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+          hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+      ])
 
-      hostingController.didMove(toParent: self)
+
+    hostingController.didMove(toParent: self)
   }
 }
 
