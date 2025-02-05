5b25eda384cb056cd4a511886b415d7ba20b7711
Dmytro Khludkov <dmytro.khludkov@vgs.io>
2025-02-05 11:09:05 +0200

    Bump blinkcard version.

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
       .package(
             name: "BlinkCard",
             url: "https://github.com/blinkcard/blinkcard-swift-package",
-            .exact("2.10.0")
+            .exact("2.10.1")
       )
     ],
     targets: [
